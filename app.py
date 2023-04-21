428b9807d0bb928b1bd295403a7e030b6dad2434
Yurii Hamii <58522152+YuriiHamii@users.noreply.github.com>
2023-04-21 12:32:22 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Product(BaseModel):
+    name: Union[str, None] = None
+    description: Union[str, None] = None
+    price: Union[float, None] = None
+    tax: Union[float, None] = None
+    tags: list[str] = []
+  # tax: float = 10.5                       # Default Values
+
+products = {
+    "1": {"name": "CocaCola", "price": 1.2},
+    "2": {"name": "PepsiCola", "description": "Orange", "price": 1.5, "tax": 0.2},
+    "3": {"name": "Fanta", "description": None, "price": 1.8, "tax": 0.3, "tags": []},
+}
+
+
+@app.get("/products/{product_id}", response_model=Product)
+async def read_product(product_id: str):
+    return products[product_id]
+
+
+@app.put("/products/{product_id}", response_model=Product)
+async def update_product(product_id: str, product: Product):
+    update_product_encoded = jsonable_encoder(product)
+    products[product_id] = update_product_encoded
+    return update_product_encoded
+
+
+@app.get("/products")
+async def get_products():
+    return products
+
+# @app.post("/products", status_code=201)
+# async def add_products(product: Product):
+    # products.append(product)
+    # return product
+
+@app.patch("/products/{product_id}", response_model=Product)
+async def update_product(product_id: str, product: Product):
+    stored_product_data = products[product_id]
+    stored_product_model = Product(**stored_product_data)
+    update_data = product.dict(exclude_unset=True)
+    updated_product = stored_product_model.copy(update=update_data)
+    products[product_id] = jsonable_encoder(updated_product)
+    return updated_product
+
+    
+# uvicorn app:app --reload
